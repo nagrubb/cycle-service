0336ce490904f66d4df6eeeb26753d5775c201c4
Nathan Grubb <me@nathangrubb.io>
2020-01-20 02:06:36 +0700

    Fix cycle API

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,19 @@
-FROM ubuntu:latest
-MAINTAINER Nathan Grubb "me@nathangrubb.io"
+FROM ubuntu:18.04
+MAINTAINER Nathan Grubb "nate@nathangrubb.io"
 
+# Install System Dependencies
 RUN apt-get update -y
 RUN apt-get install -y python3-pip
 
-COPY . /service
-WORKDIR /service
+# Create Service Directory
+RUN mkdir -p /opt/cycle/
+WORKDIR /opt/cycle
 
+# Install Service Dependencies
+COPY requirements.txt /opt/cycle
 RUN pip3 install -r requirements.txt
+COPY cycle.py /opt/cycle
 
+# Run Service
 ENTRYPOINT ["python3"]
 CMD ["-u", "cycle.py"]
--- cycle.py
+++ cycle.py
@@ -3,18 +3,32 @@ import os
 from flask import Flask, jsonify
 from stravalib import unithelper
 from stravalib.client import Client
+from stravalib.exc import AccessUnauthorized
+
+def read_environ_file(env_name):
+    filename = os.environ[env_name]
+    return open(filename, 'r').read()
+
+def get_access_token():
+    client = Client()
+    refreshed_token_info = client.refresh_access_token(
+        client_id, client_secret, refresh_token)
+    new_access_token = refreshed_token_info['access_token']
+    if refresh_token != refreshed_token_info['refresh_token']:
+        print(
+            'Refresh token does not match existing, this will most definitely cause issues in the future')
+    return new_access_token
 
 init_error = None
 application = Flask(__name__)
 
 try:
-    token_filename = os.environ['STRAVA_ACCESS_TOKEN_FILE']
-    token_filename = '/run/secrets/strava_access_token'
-    access_token = open(token_filename, 'r').read()
+    client_id = read_environ_file("STRAVA_CLIENT_ID_FILE")
+    client_secret = read_environ_file("STRAVA_CLIENT_SECRET_FILE")
+    refresh_token = read_environ_file("STRAVA_REFRESH_TOKEN_FILE")
     year_goal = int(os.environ['YEAR_GOAL'])
 except Exception as e:
     init_error = str(e)
-
 
 @application.route("/api/v1/cycle")
 def cycle():
@@ -22,9 +36,10 @@ def cycle():
         if init_error:
             return jsonify(error=init_error), 500
 
-        client = Client()
-        client.access_token = access_token
+        access_token = get_access_token()
+        client = Client(access_token)
         stats = client.get_athlete_stats()
+        print(stats)
         ride_total = float(unithelper.miles(stats.ytd_ride_totals.distance))
 
         return jsonify(
@@ -32,7 +47,7 @@ def cycle():
             goal=year_goal
         )
     except Exception as e:
-        return jsonify(error = str(e)), 500
+        return jsonify(error=str(e)), 500
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=80)
